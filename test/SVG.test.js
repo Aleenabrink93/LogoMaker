29e2bbd8619d5cf2b40fa9701d9208cdaeb23b72
Aleenabrink93 <aleenabrink@gmail.com>
2023-06-25 11:08:30 -0600

    added tests for shape.js

--- test/SVG.test.js
+++ test/SVG.test.js
@@ -1,20 +1,20 @@
 const Svg = require("../lib/svg");
 const{Square} = require("../lib/shape");
 
-
+//test size
 test("should render a 300x200 svg", ()=>{
     const testSVG = new Svg()
     const expectedSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"></svg>`
     expect(testSVG.render()).toEqual(expectedSVG)
 })
-
+// test text
 test("should append text element", ()=>{
     const testSVG = new Svg();
     testSVG.setText("A","white");
     const expectedSvg =
     '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">A</text></svg>';
 })
-
+// test shape
 test("should append the shape",()=>{
     const testSVG = new Svg();
     const testShape = new Square;
--- test/Shape.test.js
+++ test/Shape.test.js
@@ -1,31 +1,31 @@
-const { describe } = require("yargs")
+const { Circle, Triangle, Square } = require("../lib/shape");
 
-describe("circle",()=>{
+//test circle
+describe("Circle", () => {
+    it("should render svg for a green circle elementt",()=>{
+        const shape = new Circle();
+        shape.setColor("green");
+        expect(shape.render()).toEqual(
+            '<circle cx="150" cy="100" r="80" fill="green" />'
+        );
+    })});
 
-// test("should render svg for a green circle element", () => {
-//     const expectedSvg = '<circle cx="150" cy="100" r="80" fill="green" />';
-
-//     test("should accept a fillColor param", () => {
-//         const expectedSvg = '<circle cx="150" cy="100" r="80" fill="blue" />';
-
-    
-})
-
-describe("triangle",()=>{
-    // test("should render svg for a green polygon element", () => {
-    //     const expectedSvg =
-    //       '<polygon points="150, 18 244, 182 56, 182" fill="bisque" />';
-    // test("should accept a fillColor param", () => {
-    //         const expectedSvg =
-    //           '<polygon points="150, 18 244, 182 56, 182" fill="purple" />';  
-})
-
-describe("square",()=>{
-    // test("should accept a fillColor param", () => {
-    //     const expectedSvg =
-    //       '<rect x="90" y="40" width="120" height="120" fill="red" />';
-    // test("should render svg for a green polygon element", () => {
-    //     const expectedSvg =
-    //       '<rect x="90" y="40" width="120" height="120" fill="dodgerblue" />';
-
-})
+// test triangle
+describe("Triangle", () => {
+    it("should render svg for a purple polygon element",()=>{
+        const shape = new Triangle();
+        shape.setColor("purple");
+        expect(shape.render()).toEqual(
+          '<polygon points="150, 18 244, 182 56, 182" fill="purple" />'
+        );
+    })});
+  
+// test square
+describe("Square", () => {
+    it("should render svg for a red square/rectangle element",()=>{
+        const shape = new Square();
+        shape.setColor("red");
+        expect(shape.render()).toEqual(
+            '<rect x="90" y="40" width="120" height="120" fill="red" />'
+        );
+    })});
